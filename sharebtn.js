fb3bcbf72efda574e4ed849cfb450ce683c0f380
emsumit <72928644+emsumit@users.noreply.github.com>
2024-01-08 20:55:46 +0530

    0.6

--- sharebtn.js
+++ sharebtn.js
@@ -8,22 +8,3 @@ window.addEventListener("scroll", function () {
     shareButton.style.top = "50%";
   }
 });
-
-// window.addEventListener("scroll", function () {
-//   var shareButton = document.querySelector(".sharebtn");
-//   var viewportHeight = window.innerHeight;
-//   var triggerHeight = viewportHeight / 2;
-//   var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-//   if (scrollPosition >= triggerHeight) {
-//     // Make the share button sticky
-//     shareButton.style.position = "fixed";
-//     shareButton.style.bottom = "20px"; // Position from the bottom of the viewport
-//     shareButton.style.right = "20px"; // Position from the right of the viewport
-//   } else {
-//     // Reset to original position
-//     shareButton.style.position = "absolute"; // or 'static', depending on your initial layout
-//     shareButton.style.bottom = ""; // Reset bottom position
-//     shareButton.style.right = ""; // Reset right position
-//   }
-// });
